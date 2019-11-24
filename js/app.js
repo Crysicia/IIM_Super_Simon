8b7fa02c9409997c8249b547fd7e3c41bd70db02
Crysicia <7459685+Crysicia@users.noreply.github.com>
2019-11-24 22:18:41 +0100

    :sparkles: Fix modals

--- js/app.js
+++ js/app.js
@@ -24,8 +24,7 @@ class Sequence {
 
 class Button {
   constructor(id, sound) {
-    this.id = id;
-    this.element = document.querySelector(`#${this.id}`);
+    this.element = document.querySelector(id);
     this.sound = new Audio(sound);
   }
 
@@ -51,8 +50,9 @@ class Game {
   build() {
     this.turn = 0;
     this.speed = 1200;
-    this.buttons = this.elements['buttons'];
-    this.scoreboard = this.elements['scoreboard'];
+    this.buttons = this.elements.buttons;
+    this.gameOverModal = this.elements.modals.gameOver;
+    this.scoreboard = this.elements.scoreboard;
     this.sequence = new Sequence(Object.keys(this.buttons).length);
     this.playerTurn = false;
     this.playerSequence = [];
@@ -71,7 +71,7 @@ class Game {
     const actualSequence = this.sequence.generate();
     this.turn++;
 
-    this.scoreboard['turn'].innerHTML = this.turn;
+    this.displayTurn(this.turn);
 
     if (this.turn <= 8) { this.speed -= 100; }
 
@@ -83,61 +83,106 @@ class Game {
     });
   }
 
-  over() {
-    this.playerTurn = false;
-    console.error('Game over!');
-  }
-
   addToPlayerSequence(button) {
     this.playerSequence.push(parseInt(button));
 
-    this.scoreboard['streak'].innerHTML = this.playerSequence.length;
+    this.displayStreak(this.playerSequence.length);
 
     if (!this.sequence.match(this.playerSequence)) { this.over(); return; }
     if (this.playerSequence.length === this.sequence.length()) {
       this.playerTurn = false;
-      this.scoreboard['whoseturn'].innerHTML = "Simon's turn!";
-      this.scoreboard['streak'].innerHTML = 0;
+      this.scoreboard.whoseturn.innerHTML = "Simon's turn!";
+      this.displayStreak(0);
       setTimeout(() => { this.nextTurn(); }, 1200);
     }
+  }
+
+  displayTurn(turn) {
+    this.scoreboard.turns.forEach((e) => { e.innerHTML = turn; });
+  }
+
+  displayStreak(streak) {
+    this.scoreboard.streaks.forEach((e) => { e.innerHTML = streak; });
   }
 
   userTurn() {
     this.playerSequence = [];
     this.playerTurn = true;
-    this.scoreboard['whoseturn'].innerHTML = "Your turn!";
+    this.scoreboard.whoseturn.innerHTML = 'Your turn!';
+  }
+
+  over() {
+    this.playerTurn = false;
+    this.gameOverModal.show();
   }
 
   reset() {
-    this.scoreboard['turn'].innerHTML = 0;
-    this.scoreboard['streak'].innerHTML = 0;
+    this.displayTurn(0);
+    this.displayStreak(0);
     this.build();
   }
 }
 
+class Modal {
+  constructor(id, open, close) {
+    this.window = document.querySelector(id);
+    this.close = document.querySelector(`${id} ${close}`);
+    this.close.addEventListener('click', () => {
+      this.hide();
+    });
+
+    // Open is an optionnal parameter
+    if (open) {
+      this.open = document.querySelector(open);
+      this.open.addEventListener('click', () => {
+        this.show();
+      });
+    }
+  }
+
+  show() {
+    this.window.style = 'display: block';
+  }
+
+  hide() {
+    this.window.style = '';
+  }
+}
+
+
 // Init game
 
-const $red = new Button('red', 'audio/do.mp3');
-const $blue = new Button('blue', 'audio/re.mp3');
-const $green = new Button('green', 'audio/mi.mp3');
-const $yellow = new Button('yellow', 'audio/fa.mp3');
-
-const $turnDisplay = document.querySelector('#turn-display');
-const $whoseTurnDisplay = document.querySelector('#whoseturn');
-const $streakDisplay = document.querySelector('#streak-display');
+// Buttons
+const red = new Button('#red', 'audio/do.mp3');
+const blue = new Button('#blue', 'audio/re.mp3');
+const green = new Button('#green', 'audio/mi.mp3');
+const yellow = new Button('#yellow', 'audio/fa.mp3');
 
 const buttons = {
-  1: $red, 2: $blue, 3: $green, 4: $yellow,
+  1: red, 2: blue, 3: green, 4: yellow,
 };
 
+// Display
+const $turnDisplays = document.querySelectorAll('#turn-display');
+const $whoseTurnDisplay = document.querySelector('#whoseturn');
+const $streakDisplays = document.querySelectorAll('#streak-display');
+
 const scoreboard = {
-  turn: $turnDisplay, streak: $streakDisplay, whoseturn: $whoseTurnDisplay,
-}
+  turns: $turnDisplays, streaks: $streakDisplays, whoseturn: $whoseTurnDisplay,
+};
+
+// Modals
+const gameOverModal = new Modal('#gameover', '', '.close');
+const rulesModal = new Modal('#gameover', '#infos', '.close');
+
+const modals = {
+  gameOver: gameOverModal,
+};
+
 
 const domElements = {
-  buttons: buttons, scoreboard: scoreboard,
-}
-
+  buttons, scoreboard, modals,
+};
 
 const game = new Game(domElements);
 game.nextTurn();
